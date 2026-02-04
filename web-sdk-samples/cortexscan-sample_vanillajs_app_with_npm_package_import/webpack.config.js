370bee55f76056c575c4936db72524e0f93eac99
Skanda Kumar Srikanta <skanda_srikanta@bradycorp.com>
2026-02-04 14:30:48 -0500

    Deploying Vanill JS sample app using package to github pages

--- web-sdk-samples/cortexscan-sample_vanillajs_app_with_npm_package_import/webpack.config.js
+++ web-sdk-samples/cortexscan-sample_vanillajs_app_with_npm_package_import/webpack.config.js
@@ -1,80 +1,88 @@
 const path = require("path")
 const HtmlWebpackPlugin  = require('html-webpack-plugin');
 
-module.exports = {
-    mode : "development",
-    entry : {
-        //Setting the input directory
-        bundle : path.resolve(__dirname, "src/index.js")
-    },
-    output : {
-        //Setting the path relative
-        path : path.resolve(__dirname, 'dist'),
-        //To create a hash next to name
-        filename : "[name][contenthash].js",
-        //To delete the previous build
-        clean: true,
-        assetModuleFilename : '[name][ext]'
-    },
-    //Helps for debugging by having source line numbers
-    devtool : 'source-map',
-    devServer : {
-        //hot reloading dev server
+module.exports = (env, argv) => {
 
-        static : {
-            directory : path.resolve(__dirname , 'dist')
+    const isProduction = argv.mode === 'production';
+
+    return {
+        mode : isProduction ? 'production' : 'development',
+        entry : {
+            //Setting the input directory
+            bundle : path.resolve(__dirname, "src/index.js")
         },
-        port : 8000,
-        //Automatically open a browser tab
-        open : true,
-        //Auto update based on changes
-        hot : true,
-        compress : true,
-        historyApiFallback: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    'style-loader','css-loader','sass-loader'
-                ]
+        output : {
+            //Setting the path relative
+            path : path.resolve(__dirname, 'dist'),
+            //To create a hash next to name
+            filename : "[name][contenthash].js",
+            //To delete the previous build
+            clean: true,
+            assetModuleFilename : '[name][ext]',
+            publicPath: isProduction 
+                ? '/cortexdecoder-web-sdk/cortexscan-sample_vanillajs_app_with_npm_package_import/' 
+                : '/'
+        },
+        //Helps for debugging by having source line numbers
+        devtool : 'source-map',
+        devServer : {
+            //hot reloading dev server
+
+            static : {
+                directory : path.resolve(__dirname , 'dist')
             },
-            {
-                test : /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader : 'babel-loader',
-                    // options : {
-                    //     presets : ['@babel/preset-env']
-                    // }
-                }
-            },
-            {
-                test : /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource'
-            },
-            // wasm files should not be processed but just be emitted and we want
-            // to have their public URL.
-            {
-            test: /\.(wasm)$/,
-            type: `javascript/auto`,
-            loader: `file-loader`,
-            // options: {
-            // if you add this, wasm request path will be https://domain.com/publicpath/[hash].wasm
-            //   publicPath: `static/`,
-            // },
-            },
+            port : 8000,
+            //Automatically open a browser tab
+            open : true,
+            //Auto update based on changes
+            hot : true,
+            compress : true,
+            historyApiFallback: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(scss|css)$/,
+                    use: [
+                        'style-loader','css-loader','sass-loader'
+                    ]
+                },
+                {
+                    test : /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader : 'babel-loader',
+                        // options : {
+                        //     presets : ['@babel/preset-env']
+                        // }
+                    }
+                },
+                {
+                    test : /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: 'asset/resource'
+                },
+                // wasm files should not be processed but just be emitted and we want
+                // to have their public URL.
+                {
+                test: /\.(wasm)$/,
+                type: `javascript/auto`,
+                loader: `file-loader`,
+                // options: {
+                // if you add this, wasm request path will be https://domain.com/publicpath/[hash].wasm
+                //   publicPath: `static/`,
+                // },
+                },
+            ]
+        },
+        resolve : {
+            extensions : ['.ts', '.js'],
+        },
+        plugins : [
+            new HtmlWebpackPlugin({
+                title : 'Code Corp Camera Scan',
+                filename : 'index.html',
+                template : 'src/template.html'
+            })
         ]
-    },
-    resolve : {
-        extensions : ['.ts', '.js'],
-    },
-    plugins : [
-        new HtmlWebpackPlugin({
-            title : 'Code Corp Camera Scan',
-            filename : 'index.html',
-            template : 'src/template.html'
-        })
-    ]
+    }
 }
