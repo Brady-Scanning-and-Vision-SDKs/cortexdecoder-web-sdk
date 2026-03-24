cfe8c9c7b5130ffb324a64b5328bfe64078ae9cd
skanda-srikanta <skanda_srikanta@bradycorp.com>
2026-03-24 10:40:44 -0400

    Delete web-sdk-samples/cortexscan-sample_vanillajs_app_with_script_tag_import directory

--- web-sdk-samples/cortexscan-sample_vanillajs_app_with_script_tag_import/README.md
+++
@@ -1,1 +0,0 @@
-[**Try Me →**](https://brady-scanning-and-vision-sdks.github.io/cortexdecoder-web-sdk/cortexscan-sample_vanillajs_app_with_script_tag_import)
--- web-sdk-samples/cortexscan-sample_vanillajs_app_with_script_tag_import/codecorp-web_sdk-2.6.0/package/dist/node/5a6e25a0be12f202b6d8.js
+++
@@ -1,1 +0,0 @@
-import CortexDecoderLib from"./libCortexDecoder.js";import CortexDecoderModule from"./libCortexDecoder.wasm";function getWasm(e){return CortexDecoderLib({locateFile:o=>o.endsWith(".wasm")?e?e.endsWith("/")?e+CortexDecoderModule.split("/").pop():e+"/"+CortexDecoderModule.split("/").pop():CortexDecoderModule:o})}export default getWasm;
